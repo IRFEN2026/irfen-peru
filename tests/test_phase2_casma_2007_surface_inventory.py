2826ac8df32e37e724a79e57d09e70022ba4890b
IRFEN2026 <marcabalonso@gmail.com>
2026-09-26 04:46:14 +0200

    fix: repair Casma inventory regression test syntax

--- tests/test_phase2_casma_2007_surface_inventory.py
+++ tests/test_phase2_casma_2007_surface_inventory.py
@@ -21,7 +21,8 @@ def test_casma_2007_surface_inventory_is_fail_closed():
         "1375961","1375962","1375963","1375964","1375965","1375966","1375967","1375968","1375969"
     ]
     assert [row["quebradas"] for row in inventory["n7_units"]] == [58,191,64,113,102,27,1,19,51]
-    assert [row["annex_map"] for row in inventory["n7_units"]] == ["13.1","13.2","13.3","13.4","13.5","13.6","13.6","13.7","13.8"]\n    assert data["provenance"]["annex_map_index_qa"]["verified_sequence"][-1] == "13.8:1375969"
+    assert [row["annex_map"] for row in inventory["n7_units"]] == ["13.1","13.2","13.3","13.4","13.5","13.6","13.6","13.7","13.8"]
+    assert data["provenance"]["annex_map_index_qa"]["verified_sequence"][-1] == "13.8:1375969"
     assert data["recovery_value"]["exact_vector_recovered"] is False
     assert data["recovery_value"]["pdf_digitization_authorized"] is False
     assert data["map_policy"]["map_eligible"] is False
